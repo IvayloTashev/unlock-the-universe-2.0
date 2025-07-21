492550fa3a9c34009cbef84426b2be4f59dc50cb
IvayloTashev <i.tashev@abv.bg>
2025-07-21 21:19:39 +0300

    Add hero component desktop and mobile

---
+++ client/src/components/hero/Hero.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import solarSystem from "../../assets/solar-system.png";
+import solarSystem2 from "../../assets/solar-system2.png";
+import solarSystem3 from "../../assets/solar-system3.png";
+import skyWithStars from "../../assets/skyWithStars.png";
+import useMediaQuery from "../../hooks/useMediaQuery";
+import HeroText from "./HeroText";
+
+const Hero = () => {
+  const isAboveMediumScreen = useMediaQuery("(min-width: 768px)");
+
+  return (
+    <section>
+      {isAboveMediumScreen ? (
+        <div className="h-screen overflow-hidden">
+          <img src={solarSystem2} alt="solarSystem" className="mx-auto w-full opacity-60" />
+          <HeroText position="top-1/2" />
+        </div>
+      ) : (
+        <div className="relative">
+          <img src={skyWithStars} alt="skyWithStars" className="opacity-40" />
+          <img
+            src={solarSystem3}
+            alt="solarSystem"
+            className="absolute top-80 mx-auto mt-5 opacity-70"
+          />
+          <HeroText position="top-1/2" />
+        </div>
+      )}
+    </section>
+  );
+};
+
+export default Hero;
