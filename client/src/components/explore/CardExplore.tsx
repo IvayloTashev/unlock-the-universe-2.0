cd5deed6de7e5083336ee715fe0b58f8027481f2
IvayloTashev <i.tashev@abv.bg>
2025-07-23 18:49:58 +0300

    Add heading text for explore component

--- client/src/components/explore/CardExplore.tsx
+++ client/src/components/explore/CardExplore.tsx
@@ -1,21 +1,25 @@
-import React from 'react'
+import React from "react";
 
 type Props = {
-    image: string,
-    children: React.ReactNode;
-}
+  image: string;
+  children: React.ReactNode;
+};
 
-const CardExplore = ({image, children}: Props) => {
+const CardExplore = ({ image, children }: Props) => {
   return (
-<div className="w-full sm:w-[45%] md:w-[30%] lg:w-[25%] p-5 bg-card rounded-3xl border border-gray-100/20 group transition-all">
-    <div className="flex flex-col items-center gap-5">
-      <img src={image} alt={`${image}`} className="w-full max-w-[200px] group-hover:scale-110 transition duration-500" />
-      <p className="text-white text-center text-3xl sm:text-4xl md:text-5xl font-bebas">{children}</p>
+    <div className="w-full sm:w-[45%] md:w-[30%] lg:w-[25%] p-5 bg-card rounded-3xl border border-gray-100/20 group transition-all">
+      <div className="flex flex-col items-center gap-5">
+        <img
+          src={image}
+          alt={`${image}`}
+          className="w-full max-w-[200px] group-hover:scale-110 transition duration-500"
+        />
+        <p className="text-white text-center text-3xl sm:text-4xl md:text-5xl font-bebas">
+          {children}
+        </p>
+      </div>
     </div>
-  </div>
-  )
-}
+  );
+};
 
-export default CardExplore
-
-
+export default CardExplore;
