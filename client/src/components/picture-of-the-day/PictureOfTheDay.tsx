b0df44b66728eb32a8faaccdb7abd95887375d54
IvayloTashev <i.tashev@abv.bg>
2025-07-26 15:00:49 +0300

    Add Picture of the day component

---
+++ client/src/components/picture-of-the-day/PictureOfTheDay.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+import { useGetPod } from "../../hooks/useGetPod";
+
+const PictureOfTheDay = () => {
+  const nasaPodData = useGetPod();
+  console.log(nasaPodData);
+
+  return (
+    <section>
+      {nasaPodData?.media_type === "image" ? (
+        <div className="p-5 flex flex-col gap-8 justify-center items-center">
+          <div className="">
+            <img
+              src={nasaPodData.url}
+              alt="nasaPictureOfTheDay"
+              className="rounded-3xl max-h-[70vh]"
+            />
+          </div>
+          <div className="text-text-gray text-center p-3 max-w-[90vw] md:max-w-[80vw] bg-card border-1 border-gray-100/20 rounded-3xl">
+            <h1 className="text-2xl font-bold">{nasaPodData.title}</h1>
+            <p className="mt-3">{nasaPodData.explanation}</p>
+          </div>
+        </div>
+      ) : (
+        <div>
+          <div>
+            <iframe
+              src={nasaPodData?.url}
+              title="YouTube video"
+              allowFullScreen
+              className="w-full h-full"
+            />
+          </div>
+          <div className="text-text-gray">
+            <h1>{nasaPodData?.title}</h1>
+            <p>{nasaPodData?.explanation}</p>
+          </div>
+        </div>
+      )}
+    </section>
+  );
+};
+
+export default PictureOfTheDay;
