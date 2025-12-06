3c5e7fc120d181c894f49aeda66b51a3588af5bf
IvayloTashev <i.tashev@abv.bg>
2025-12-06 11:25:22 +0200

    Update nasaAPI.ts

--- client/src/api/nasaAPI.ts
+++ client/src/api/nasaAPI.ts
@@ -1,6 +1,7 @@
 import type { NasaPodType } from "../types";
 import { get } from "./requester"
 
-const NASA_API = `https://api.nasa.gov/planetary/apod?api_key=${import.meta.env.VITE_NASA_API_KEY}`;
+const NASA_API = `https://api.nasa.gov/planetary/apod?api_key=z2yv1z4qkLlqevZsY40ZoZmpuJLqWr1ad5NcwBe4`;
+// const NASA_API = `https://api.nasa.gov/planetary/apod?api_key=${import.meta.env.VITE_NASA_API_KEY}`;
 
 export const getNasaPodData = (): Promise<NasaPodType> => get(NASA_API);
